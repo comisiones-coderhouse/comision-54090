db6f9b3353db8ca8557575381b10f5be546a57cb
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-03-14 01:01:18 -0300

    Clase 2 : Instalacion con vite

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+/* 
+
+import <nombre_variable> from "algun-archivo" 
+
+*/
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.scss'
+
+
+const root = document.getElementById('root')
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
