cebde99a7fcc36f067cdb20ed14279a84cb0271a
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-03-28 00:45:27 -0300

    Clase Promesas y .map

--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -1,26 +1,30 @@
 import { Button } from "antd";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+function UserCard({ name }) {
 
-function UserCard({ name , age , isDev }) {
-    /*
-    const elResultadoDelHook = useState("Horacio")//["Horacio", fn(){}]
-    const nombre = elResultadoDelHook[0]//undefined
-    const setNombre = elResultadoDelHook[1] 
-    */
-    const [nombre, setNombre] = useState("Horacio")
+    const [nombre, setNombre] = useState(name)
 
-    const handleClick = () => {
+    function handleClick() {
         setNombre("Carlos")
     }
+
+    /* useEffect(function) */
+    /* useEffect(function,[]) */
+
+    /* useEffect(() => {
+        window.addEventListener("resize", () => {
+            console.log("width", window.innerWidth)
+        })
+    }, []) */
+
 
     return (
         <div className="p-4 transition rounded-md shadow-md user-card hover:scale-105 group">
             <div className="overflow-hidden aspect-video">
                 <img className="w-full transition-all duration-500 rounded-md grayscale group-hover:grayscale-0 group-hover:scale-150" src="https://picsum.photos/400" alt="card image" />
             </div>
-            <h2 className="my-2 font-bold">{name}</h2>
-            <p className="text-sm">{age} años</p>
+            <h2 className="my-2 font-bold">{nombre}</h2>
             <Button onClick={handleClick}>ver mas</Button>
         </div>
     )
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,17 +3,55 @@ import UserCard from '../UserCard'
 import Timer from "../Timer"
 import UserList from "../UserList"
 
+
+
+
+
+const miPromesa = new Promise((res, rej) => { 
+    console.log("Pidiendo...")
+    setTimeout(()=>{
+        res()
+    },5000)
+})
+
+miPromesa.then(() => { console.log("Bien") })
+
+miPromesa.catch(() => { console.log("Mal") })
+
+miPromesa.finally(() => { console.log("Finalizo") })
+
+
+
+
+
+
 function Main() {
+
+    const users = ["Horacio", "Carlos", "Ana", "Claudia", "Eddy"]
+
     return (
         <Page title="Home">
             <section className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 users">
+
                 {/* 
-                <UserCard name="pedro" age="34" isDev={true}/>
-                <UserCard name="Ana" age="23" isDev={false} />
-                <UserCard name="Claudia" age="45" isDev={true}/> 
+                
+                {users.map((user, i) => {
+                    return (
+                        <div key={i}>
+                            <p>{user}</p>
+                            <p> soy la edad </p>
+                        </div>
+                    )
+                })} 
+                
                 */}
+
+                {users.map((user, i) => {
+                    return <UserCard key={i} name={user} />
+                })}
+
                 {/* <Timer/> */}
-                <UserList/>
+                {/* <UserList /> */}
             </section>
         </Page>
     )
